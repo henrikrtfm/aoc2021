56cbaee968781fa575b3889d9d934133ee50457a
Henrik Edsparr <henrik.edsparr@gmail.com>
2021-12-01 15:18:06 +0100

    Solution for Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,15 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        val listPairs = input.zipWithNext()
+        return listPairs.filter { it.first < it.second }.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val listWindow = input.windowed(3).map { it.sum() }
+        return part1(listWindow)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
