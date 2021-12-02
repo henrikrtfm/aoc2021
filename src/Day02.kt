9508ef00e42e7926756ef1d67ca7d8d1ccfefb6e
Henrik Edsparr <henrik.edsparr@gmail.com>
2021-12-02 09:10:04 +0100

    Solution for Day2

---
+++ src/Day02.kt
@@ -0,0 +1,48 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val commandList = input.map(Command::parse)
+        var horizontal = 0
+        var depth = 0
+        commandList.forEach {
+            when(it.direction){
+                "forward" -> horizontal += it.value
+                "up" -> depth -= it.value
+                "down" -> depth += it.value
+            }
+        }
+        return horizontal*depth
+    }
+
+    fun part2(input: List<String>): Int {
+        val commandList = input.map(Command::parse)
+        var horizontal = 0
+        var depth = 0
+        var aim = 0
+        commandList.forEach {
+            when(it.direction){
+                "forward" -> {
+                    horizontal += it.value
+                    depth += aim*it.value
+                }
+                "up" -> aim -= it.value
+                "down" -> aim += it.value
+            }
+        }
+        return horizontal*depth
+    }
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
+data class Command(
+    val direction: String,
+    val value: Int
+){
+    companion object {
+        fun parse(line: String) = Command(
+            direction = line.substringBefore(" "),
+            value = line.substringAfter(" ").toInt()
+        )
+    }
+}
