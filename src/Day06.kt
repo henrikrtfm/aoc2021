4a67bd4b41ad5f6c16a7d80dbb61bd01e1ffeb41
Henrik Edsparr <henrik.edsparr@gmail.com>
2021-12-07 21:59:07 +0100

    Solution for Day6

---
+++ src/Day06.kt
@@ -0,0 +1,28 @@
+fun main() {
+
+    fun updateState(input: Int): List<Int>{
+        return when(input){
+            0 -> listOf(6, 8)
+            else -> listOf(input-1)
+        }
+    }
+
+    fun simulateDays(initialstate: List<Int>, days: Int): List<Int>{
+        return (1..days).fold(initialstate) { newstate, _ -> newstate.flatMap { currentstate -> updateState(currentstate) }}
+    }
+
+    fun part1(input: List<Int>): Int {
+        val answer = simulateDays(input, 80)
+        return answer.size
+    }
+
+    fun part2(input: List<Int>): Int {
+        val answer = simulateDays(input, 256)
+        return answer.size
+    }
+
+    val input = readInput("Day06")
+    val initialstate = input.flatMap{it.split(",").map{ itx -> itx.toInt()}}
+    //println(part1(initialstate))
+    println(part2(initialstate))
+}
